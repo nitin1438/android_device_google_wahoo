7956cc799e3f6ff11b938dd0f95831071b6fafe7
Dan Pasanen <dan.pasanen@gmail.com>
2017-02-01 16:25:54 -0600

    taimen: include vzw-specific sepolicy
    Change-Id: I32e89f0458efbb0ee25e550b91cd1b9fb3d2b2c0
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -4,3 +4,6 @@ TARGET_KERNEL_SOURCE := kernel/google/wahoo
 BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_NEEDS_DTBOIMAGE := true
+
+# Sepolicy
+BOARD_SEPOLICY_DIRS += device/google/marlin/sepolicy/verizon

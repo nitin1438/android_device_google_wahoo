9a7a86742eb04c597bda929ee6e66c7412009481
Dan Pasanen <dan.pasanen@gmail.com>
2017-08-22 09:41:36 -0500

    taimen: always add manufacturer and model to recordings
    Signed-off-by: Joe Maples <joe@frap129.org>

--- device-carbon.mk
+++ device-carbon.mk
@@ -1,2 +1,5 @@
 # Carbon specific carbon device config
 DEVICE_PACKAGE_OVERLAYS += device/google/wahoo/overlay-carbon
+
+PRODUCT_PROPERTY_OVERRIDES += \
+      media.recorder.show_manufacturer_and_model=true

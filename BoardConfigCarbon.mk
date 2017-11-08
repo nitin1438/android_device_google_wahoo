44fe48e27f38498b269a1aedc36c199532b5d8e3
Joe Maples <joe@frap129.org>
2017-11-08 17:44:47 -0500

    wahoo: Add inline kernel building support
    Change-Id: I642ce2baddd14efde19e8a4d0a89a1c03c3d055b
    Signed-off-by: Joe Maples <joe@frap129.org>

--- BoardConfigCarbon.mk
+++ BoardConfigCarbon.mk
@@ -1,1 +1,6 @@
 # Custom Carbon BoardConfig for wahoo
+# Inline kernel building
+TARGET_KERNEL_SOURCE := kernel/google/wahoo
+BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_NEEDS_DTBOIMAGE := true

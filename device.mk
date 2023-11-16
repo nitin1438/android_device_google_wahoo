b9843921f0e1fd63b4f9f4ea38718af101c79ec5
0xCAFEBABE <me.cafebabe@gmail.com>
2023-11-16 19:49:21 +0800

    wahoo: Switch to legacy WiFi HIDL HAL
    * With the new WiFi AIDL HAL, the framework thinks the device
      supports interface combination for some unknown reason,
      even though the service doesn't advertise it. As a result,
      the WiFi service fails on configureChip method.
    
    Change-Id: I0c3fa7b4c947c3525c30f7cef0b2674bee224da7

--- device.mk
+++ device.mk
@@ -497,7 +497,7 @@ PRODUCT_PACKAGES += \
 
 # Wifi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service \
+    android.hardware.wifi@1.0-service.legacy \
     wificond \
     libwpa_client \
     WifiOverlay

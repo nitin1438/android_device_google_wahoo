40be27f7990c5feedd752929040a9d91e2398d9e
Tommaso Fonda <fondatommaso2@gmail.com>
2023-11-27 15:32:40 +0100

    wahoo: Build android.hardware.radio.config@1.0-service
    Fixes lags when entering SIM settings.
    
    Change-Id: I8aa19bfacaa242fe9be7d5e25e7df54c64af5516

--- device.mk
+++ device.mk
@@ -204,6 +204,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.1.vendor \
     android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.0-service \
     libxml2
 
 # Audio fluence, ns, aec property, voice and media volume steps

6fb2abbff8e3028a560302b48a960154411927ed
Ionut Gherman <ghermanionut96@gmail.com>
2024-07-16 13:57:25 +0300

    wahoo: enable Quick Tap
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -6,6 +6,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     ElmyraService
 
+# Quick Tap
+PRODUCT_COPY_FILES += \
+    device/google/wahoo/quick_tap.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/quick_tap.xml
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    persist.columbus.use_ap_sensor=false
+
 # fastbootd
 PRODUCT_PACKAGES += \
     fastbootd

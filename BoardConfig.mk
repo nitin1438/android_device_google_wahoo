4a02e5a36d8c6ef480fb78bb989fbedeb46468ca
Nitin1438 <nitin1438@users.noreply.github.com>
2025-08-31 18:49:18 +0530

    Update path of device_framework_matrix.xml

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -154,7 +154,7 @@ DEVICE_MATRIX_FILE := $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_MANIFEST_FILE := $(COMMON_PATH)/framework_manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/device_framework_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    vendor/bliss/config/device_framework_matrix.xml
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
 # Board uses A/B OTA.
